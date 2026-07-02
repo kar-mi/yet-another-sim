65fa1bafd6114d0d5f490a40bfd59a6c4e003daa
rak-mi <someonex666@gmail.com>
2026-07-02 13:21:58 -0400

    relative east and west safe

--- src/engine/__tests__/integration/blackHoleBots.test.ts
+++ src/engine/__tests__/integration/blackHoleBots.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "bun:test";
 import { applyBotPatterns, loadBotPatterns, loadRaid } from "../../raidLoader";
 import { createWorld } from "../../world";
+import { runTicksWithComputedBotIntents } from "../helpers";
 
 test("black-hole raid and bot companion load with resolved tether frame positions", async () => {
   const raidData = Bun.YAML.parse(await Bun.file("raids/dancing-mad-ultimate/black-hole.yaml").text());
@@ -13,4 +14,14 @@ test("black-hole raid and bot companion load with resolved tether frame position
   expect(world.eventPositions["black-hole-1-laser-1"]).toBeDefined();
   expect(world.eventPositions["black-hole-2-laser-1"]).toBeDefined();
   expect(world.pendingTethers.filter(tether => tether.id.startsWith("black-hole-2-laser"))).toHaveLength(3);
+});
+
+test("black-hole bots survive the first Slap Happy side cleave", async () => {
+  const raidData = Bun.YAML.parse(await Bun.file("raids/dancing-mad-ultimate/black-hole.yaml").text());
+  const botData = Bun.YAML.parse(await Bun.file("raids/dancing-mad-ultimate/black-hole-bots.yaml").text());
+  const raid = applyBotPatterns(loadRaid(raidData), loadBotPatterns(botData));
+  const world = runTicksWithComputedBotIntents(createWorld(raid, 1), Math.ceil(31.1 * 60));
+
+  expect(world.status).toBe("running");
+  expect(world.players.every(player => player.alive)).toBe(true);
 });
